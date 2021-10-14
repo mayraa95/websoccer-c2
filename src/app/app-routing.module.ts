ac68932872b61db028a20c83916eb649898d8276
unknown <mayasyafira95@gmail.com>
2021-10-14 13:58:16 +0700

    project pertama c2 web_soccer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScoringComponent } from './scorings/scorings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'scorings', component:ScoringsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoringsComponent } from './scorings/scorings.component';
+import { StandingsComponent } from './standings/standings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoringsComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule,
